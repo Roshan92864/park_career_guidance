38df6005717f893722ddfda7d9c963478d997bab
Roshan92864 <roshana@parkcorporates.com>
2025-12-26 11:03:47 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -6,6 +6,11 @@ import os
 BASE_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
 
 def load_json(filename):
-    with open(os.path.join(BASE_PATH, filename), "r") as f:
+    path = os.path.join(BASE_PATH, filename)
+    with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
+def save_json(filename, data):
+    path = os.path.join(BASE_PATH, filename)
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
